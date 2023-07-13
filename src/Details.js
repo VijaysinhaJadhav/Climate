1de0b86c565f20066fbfee238c5f4de3f09ec055
Vijaysinha <vijaysinha.jadhav@primesophictech.com>
2023-07-14 01:26:04 +0530

    second commit

--- src/Details.js
+++ src/Details.js
@@ -11,7 +11,7 @@ const Details = ({ route }) => {
             .then(res => {
                 console.log(res)
                 setData(res)
-            }) 
+            })  
             .catch(err => console.log(err))
     }, [])
     const Details = ({title,value}) => {
